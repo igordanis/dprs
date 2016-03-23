be149d9ab1de4c71d4baffb3ab7b85a54fdcad53
Igor Danis <danis@essential-data.sk>
2016-03-23 15:38:29 +0100

    haproxy service added to docker-compose and as dockerfile

--- build.sh
+++ build.sh
@@ -9,4 +9,4 @@ docker-compose up -d
 echo "Scaling webapp to 5 nodes"
 docker-compose scale web=5
 
-# echo "info.: zabbix takes approx. 60sec to boot. Please be patient and keep refreshing :)"
+# echo "info.: zabbix takes approx. 60sec to boot. Please be patient :)"
