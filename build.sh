66921406a572e8a36fae1872dab2b58e9f1a0da1
Igor Danis <danis@essential-data.sk>
2016-03-29 23:31:32 +0200

    docker-compose-elk integrated into main docker-compose

--- build.sh
+++ build.sh
@@ -2,7 +2,9 @@
 
 #compile, package
 echo "Bulding application"
-#mvn clean package > build.log
+mvn clean package > build.log
+cp ./target/dprs-0.0.1.jar ./docker/dynamo/dprs-0.0.1.jar
+docker-compose build
 
 echo "Starting required containers"
 docker-compose up -d
--- elk-reload.sh
+++ elk-reload.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 mvn clean install
 cp ./target/dprs-0.0.1.jar ./docker/dynamo/dprs-0.0.1.jar
 
